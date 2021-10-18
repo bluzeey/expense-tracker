8fe4622fc3f29ff84f42f7ceecefb65ee98d9456
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-18 21:01:53 +0530

    Updating the app

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Card,CardHeader,CardContent,Typography} from '@material-ui/core'
+import {Doughnut} from 'react-chartjs-2'
+import useStyles from './styles'
+const Details = ({title}) => {
+    const classes=useStyles()
+    return (
+        <Card className={classes.income}>
+            <CardHeader title={title}>
+                <CardContent>
+                    <Typography variant="h5">$50</Typography>
+                    {/*<Doughnut data="DATA"/>*/}
+                </CardContent>
+            </CardHeader>
+        </Card>
+    )
+}
+
+export default Details
