b07e92b2689fda7c75722dbb4e3ac10608f40726
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-25 10:37:45 +0530

    Final Commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -15,9 +15,8 @@ const Main = () => {
                 <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
                 <Typography variant="subtitle1" style={{lineHeight:'1.5em',marginTop:'20px'}}>
                     <InfoCard/>
-                    Try saying:Add income for $100 in Category Salary for Monday...
                 </Typography>
-                <Divider/>
+                <Divider className={classes.divider}/>
                 <Form/>
             </CardContent>
             <CardContent className={classes.cardContent}>
